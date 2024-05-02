7260fad401b50d93f750283afce6dfb64a15d318
Babu Nair <babunairofficial@gmail.com>
2024-05-02 23:59:29 +0530

    process

--- Backend_1/backend/script.js
+++ Backend_1/backend/script.js
@@ -1,7 +1,9 @@
-let n = 5;
+// let n = 5;
 
-for(let i =1; i<=n;i++){
-    console.log("hello,",i);
-}
+// for(let i =1; i<=n;i++){
+//     console.log("hello,",i);
+// }
 
-console.log("bye!");
+// console.log("bye!");
+
+console.log(process.argv);
