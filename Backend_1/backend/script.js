5d21f5083b7c8f74e7a646acf16cbb880d8c5b17
Babu Nair <babunairofficial@gmail.com>
2024-05-03 18:22:43 +0530

    module exports across directories

--- Backend_1/backend/script.js
+++ Backend_1/backend/script.js
@@ -9,11 +9,15 @@
 // console.log(process.argv);
 
 /*to get some special values, through the user, to use*/
-let arg = process.argv;
-for(let i = 2; i<arg.length; i++){
-    console.log("hello to ", arg[i])
-}
+// let arg = process.argv;
+// for(let i = 2; i<arg.length; i++){
+//     console.log("hello to ", arg[i])
+// }
 
-//module.exports
-const sumValue = require("./math");
-console.log(sumValue);
+// //module.exports
+// const sumValue = require("./math");
+// console.log(sumValue);
+
+const info = require("./Fruits"); /*folder name*/
+
+console.log(info);
